532589a8b6324c403544d98a1c1e687ff8aa7544
srmcesar <srmcesar@hotmail.com>
2015-01-30 01:27:07 -0600

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Redirigir navegador */
-header(“Location: http://www.servnet.mx/”);
-/* Asegúrese de que el código que aparece a continuación no se ejecutará cuando redireccionamos.*/
-exit;
-?>
